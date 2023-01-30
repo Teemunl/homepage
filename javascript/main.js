bb1546212acae441725bbbe9c15ccf8cb1b5ea1d
Teemunl <teemu.liimatta@hotmail.com>
2023-01-30 15:36:50 +0200

    deleted irrelevant funtion

--- javascript/main.js
+++ javascript/main.js
@@ -7,7 +7,22 @@ prompt.addEventListener("click", function() {
 });
 
 container.addEventListener('mousedown', function(e) {
-  // your existing code here
+  var initialX = e.clientX;
+  var initialY = e.clientY;
+  var containerX = container.offsetLeft;
+  var containerY = container.offsetTop;
+  var diffX;
+  var diffY;
+  var mousemove = function(e) {
+    diffX = e.clientX - initialX;
+    diffY = e.clientY - initialY;
+    container.style.left = (containerX + diffX) + 'px';
+    container.style.top = (containerY + diffY) + 'px';
+  }
+  document.addEventListener('mousemove', mousemove);
+  document.addEventListener('mouseup', function() {
+    document.removeEventListener('mousemove', mousemove);
+  });
 });
 
 var closeBtn = document.querySelector('.prompt-header-close-btn');
@@ -16,6 +31,11 @@ closeBtn.addEventListener('click', function() {
 });
 
 var promptInput = document.querySelector("#prompt-input");
+/**
+ * function adds a event listener for 
+ * taking the commands that are typed in the 
+ * terminal
+ */
 promptInput.addEventListener("keydown", function(event) {
   if (event.keyCode === 13) { // 'Enter' key
     event.preventDefault();
@@ -32,6 +52,9 @@ promptInput.addEventListener("keydown", function(event) {
         }
       }
     }
+    if (userInput === "help") {
+      p.innerHTML = prompt.innerHTML + "You can write: cls to clear the screen " ;
+    }
     promptInput.value = "";
   }
 });
@@ -40,27 +63,3 @@ var terminalBtn = document.querySelector('.terminal-btn');
 terminalBtn.addEventListener('click', function(){
     document.querySelector('.prompt-container').style.display = 'block';
 });
-
-input.addEventListener("keydown", function(event) {
-  if (event.keyCode === 13) { // 'Enter' key
-    event.preventDefault();
-    var userInput = input.value;
-    var p = document.createElement("p");
-    p.innerHTML = prompt.innerHTML + " " + userInput;
-    p.classList.add("command");
-    p.style.color = "green";
-    document.querySelector(".prompt-container").appendChild(p);
-    if (userInput === "cls") {
-      var commands = document.querySelectorAll(".command");
-      for (var i = 0; i < commands.length; i++) {
-        if (commands[i].previousElementSibling === prompt) {
-          commands[i].remove();
-        }
-      }
-    }
-    prompt.innerHTML = "C:\\Users\\username>";
-    input.value = "";
-  }
-});
-input.classList.toggle('hide');
-input.classList.toggle('blink');
